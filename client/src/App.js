6db28c26bb6a999ea59f38bc4560bd0c266708c8
Elena Ruiz <elena.ruiz.bdn@gmail.com>
2020-04-08 23:46:50 +0200

    test library

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { ThemeProvider } from '@material-ui/core/styles';
+import {ui} from 'client-library-ui';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <ui.PrimaryButton>
+          TEST
+        </ui.PrimaryButton>
     </div>
   );
 }
