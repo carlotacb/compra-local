448c0981d479d2a469579a3e6a6d4c7ae4deb042
Albert Suarez <alsumo95@gmail.com>
2020-04-09 18:10:28 +0200

    Logged DB tables on /status endpoint

--- api/src/api/status.py
+++ api/src/api/status.py
@@ -1,5 +1,7 @@
-from src.helper import response
+from src.db.sqlalchemy import meta
+from src.helper import response, log
 
 
 def get():
+    log.info(f'List of DB tables: {list(meta.tables.keys())}')
     return response.make(False, response=dict(ok=True)), 200
