be1b13fd08aa91048f15b8741787b99114257648
Albert Suarez <alsumo95@gmail.com>
2020-04-09 18:04:14 +0200

    Added some log messages

--- api/src/connexion.py
+++ api/src/connexion.py
@@ -7,11 +7,15 @@ from src.db import sqlalchemy
 from src.helper import log
 
 
+log.info('Creating connexion application...')
 connexion_app = connexion.FlaskApp(__name__, specification_dir='./openapi/')
 flask_app = connexion_app.app
 flask_app.config['JSON_AS_ASCII'] = False
+log.info('Setting up OpenAPI specification...')
 connexion_app.add_api('openapi.yaml', arguments={'title': 'Compra Local API'})
+log.info('Setting up CORS...')
 CORS(flask_app)
+log.info(f'Connexion application built: [{connexion_app}]')
 
 
 @flask_app.teardown_appcontext
--- api/src/db/sqlalchemy.py
+++ api/src/db/sqlalchemy.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from src.helper import env
+from src.helper import env, log
 from src.model.user import User
 
 
 def _connect():
+    log.info('Connecting to DB...')
     url = 'postgresql://{}:{}@{}:{}/{}'.format(
         env.get_db_user(),
         env.get_db_password(),
@@ -15,11 +16,14 @@ def _connect():
         env.get_db_database())
     _engine = create_engine(url, client_encoding='utf8')
     _meta = MetaData(bind=_engine)
+    log.info('Connection satisfied.')
     return _engine, _meta
 
 
 def _create_tables(engine_object):
+    log.info('Creating tables if they do not exist...')
     User.__table__.create(bind=engine_object, checkfirst=True)
+    log.info('Tables created (or not).')
 
 
 # Connect to the database.
