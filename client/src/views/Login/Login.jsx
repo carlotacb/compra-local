ccea973da5f9ae02ef34a3d89e2a40c0c8c9e0ff
Carlota <carkbra@gmail.com>
2020-04-09 22:08:57 +0200

    login layed out, prepared for api conection

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,20 +1,107 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { PrimaryButton } from '../../shared-components/Button/PrimaryButton'
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      height: '100vh',
+    },
+    image: {
+      backgroundImage: 'url(https://source.unsplash.com/random)',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+	    justifyContent: 'center',
+	    alignContent: 'center',
+    },
+    form: {
+      width: '80%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+    registerGrid: {
+        marginTop: '15px'
+    },
+  }));
 
 export function Login() {
     const {user, setUser} = useContext(UserContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
+    const classes = useStyles();
+
+    const handleLogin = () => {
+        console.log(email + " " + password);
+        if (email === "carlota@hackupc.com" && password === "123") {
+            setError(false);
+            setUser({ email: email, password: password });
+            console.log(user)
+        } else {
+            setError(true);
+        }   
+    }
 
     return (
-        <div>
-            <h1>Entra a Compra Local</h1>
-
-            <pre>{JSON.stringify(user, null, 2)}</pre>
-            <button onClick={() => {
-                setUser({ id: 4, username: "bob", email: "me@bob.com" });
-            }}>Login</button>
-
-
-        </div>
+        <Grid container component="main" className={classes.root}>
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} className={classes.paper} elevation={6} square>
+                <form className={classes.form}>
+                    <TextField 
+                        error={error}
+                        variant="outlined" 
+                        margin="normal" 
+                        required 
+                        fullWidth 
+                        id="email" 
+                        label="Correu Electrònic" 
+                        name="email" 
+                        autoComplete="email" 
+                        onChange={(e)=>setEmail(e.target.value)}
+                        autoFocus />
+                    <TextField 
+                        error={error}
+                        variant="outlined" 
+                        margin="normal" 
+                        required 
+                        fullWidth 
+                        name="password" 
+                        label="Contrasenya" 
+                        type="password" 
+                        id="password" 
+                        onChange={(e)=>setPassword(e.target.value)}
+                        autoComplete="current-password" />
+                    <PrimaryButton onClick={() => handleLogin()}> Entra </ PrimaryButton>
+                    <Grid container className={classes.registerGrid}>
+                        <Grid item xs>
+                            {/* <Link href="#" variant="body2"> Forgot password? </Link> */}
+                        </Grid>
+                        <Grid item>
+                            <Link href="/registre"> {"Don't have an account? Sign Up"} </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+                {/*<h1>Entra a Compra Local</h1>
+                <pre>{JSON.stringify(user, null, 2)}</pre>
+                <button onClick={() => { setUser({ id: 4, username: "bob", email: "me@bob.com" });}}>Login</button> */}
+            </Grid>
+        </ Grid>
     )
 }
