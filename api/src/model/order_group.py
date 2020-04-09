7dbe340a734f7a2f262a6088cb679ddb31d9bc5a
Albert Suarez <alsumo95@gmail.com>
2020-04-09 21:44:14 +0200

    Added completed column to OrderGroup

--- api/src/model/order_group.py
+++ api/src/model/order_group.py
@@ -12,6 +12,7 @@ class OrderGroup(Base):
     __tablename__ = 'compra_local_order_group'
 
     id = db.Column(db.Integer, helper.get_sequence(__tablename__), primary_key=True)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
     helper_needed = db.Column(db.Boolean, nullable=False, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey(f'{User.__tablename__}.id'), nullable=False)
     helper_id = db.Column(db.Integer, db.ForeignKey(f'{User.__tablename__}.id'))
