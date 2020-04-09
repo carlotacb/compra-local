bbd3658d74939bc89de47efadf45d23b8ccd4a1d
Albert Suarez <alsumo95@gmail.com>
2020-04-09 21:54:52 +0200

    Added latitude/longitude into Local ORM class

--- api/src/model/local.py
+++ api/src/model/local.py
@@ -15,6 +15,8 @@ class Local(Base):
     name = db.Column(db.String(64), nullable=False)
     description = db.Column(db.String(256))
     postal_address = db.Column(db.String(256), nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
     website = db.Column(db.String(256))
     phone_number = db.Column(db.String(64))
     pick_up = db.Column(db.Boolean, nullable=False, default=True)
