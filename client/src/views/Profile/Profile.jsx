a2cdc2b833d99d2ccca06f81a7d318af944c26cd
Carlota <carkbra@gmail.com>
2020-04-10 13:52:53 +0200

    Created profile view structure with the basic information

--- client/src/views/Profile/Profile.jsx
+++ client/src/views/Profile/Profile.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import { Grid, Typography, makeStyles } from "@material-ui/core";
+import { SecondaryButton } from '../../shared-components/';
+import { ProfileBox } from "../../components";
+
+const useStyles = makeStyles((theme) => ({
+    
+}));
 
 export function Profile() {
+    const { user, setUser } = useContext(UserContext);
+    const classes = useStyles();
+
     return (
-        <h1>This is the UserProfile page</h1>
+        <Grid container direction="column" justify="space-between">
+            <Grid item>
+                <Typography variant="h1">
+                    Hola {user}!
+                </Typography>
+            </Grid>
+            <Grid item>
+                <ProfileBox name="Carlota" email="carlota@hackupc.com"/>
+            </Grid>
+            <Grid item>
+                <SecondaryButton> Canviar contrasenya </SecondaryButton>
+            </Grid>
+
+        </Grid>
     )
 }
 
