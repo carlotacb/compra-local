833ec4ea28cc4b046d99272947a3d16da3f3ca8f
Albert Suarez <alsumo95@gmail.com>
2020-04-10 21:34:09 +0200

    Implemented distance computation between locals for helpers

--- api/src/api/help.py
+++ api/src/api/help.py
@@ -1,4 +1,4 @@
-from src.helper import response
+from src.helper import response, maps
 from src.service import local as local_service
 
 
@@ -6,6 +6,7 @@ def get(latitude, longitude):
     try:
         # Get all local coordinates
         coordinates_dict = local_service.get_all_coordinates()
+        local_id_list = maps.filter_by_around(coordinates_dict, latitude, longitude)
         return response.make(error=False, response=dict(helper_list=[]))
     except Exception as e:
         return response.raise_exception(e)
