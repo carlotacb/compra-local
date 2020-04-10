7b03e9096eb17d2d83b46f92e4d3c8df5d213e24
Carlota <carkbra@gmail.com>
2020-04-10 16:51:14 +0200

    added ConfirmationDialog to the routing index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,9 +3,11 @@ import { SearchBox as SB} from './Box/SearchBox';
 import { ProfileBox as PB} from './Box/ProfileBox';
 import {ListView as LV} from './Listview/ListView';
 import { StoreCard as SC} from './Card/StoreCard';
+import { ConfirmationDialog as CD } from './Dialog/ConfirmationDialog';
 
 export const Sidebar = Sid;
 export const SearchBox = SB;
 export const ListView = LV;
-export const StoreCard =SC;
+export const StoreCard = SC;
 export const ProfileBox = PB;
+export const ConfirmationDialog = CD;
