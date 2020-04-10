ebe5b882caa587daf88c062b6f3574055eefab1d
Albert Suarez <alsumo95@gmail.com>
2020-04-10 20:48:21 +0200

    Added dummy data for ReviewLocal and ReviewUser

--- api/src/service/review_local.py
+++ api/src/service/review_local.py
@@ -1,11 +1,22 @@
 from src.db.sqlalchemy import db_session
 from src.helper import log
 from src.model.review_local import ReviewLocal
+from src.service import local as local_service
+from src.service import user as user_service
 
 
 def add_dummy_data():
     count = db_session().query(ReviewLocal.id).count()
     if count == 0:
         log.info(f'Adding dummy data for {ReviewLocal.__tablename__}...')
+        object_list = [
+            ReviewLocal(
+                punctuation=4, comment='Super bo tot!', order_id=1,
+                writer_id=user_service.get_id_by_name('Albert Suarez'),
+                local_id=local_service.get_id_by_name('Bona Fruita Busquets')
+            )
+        ]
+        db_session().bulk_save_objects(object_list)
+        db_session().commit()
     else:
         log.info(f'Skipping dummy data for {ReviewLocal.__tablename__} because is not empty.')
--- api/src/service/review_user.py
+++ api/src/service/review_user.py
@@ -1,11 +1,21 @@
 from src.db.sqlalchemy import db_session
 from src.helper import log
 from src.model.review_user import ReviewUser
+from src.service import user as user_service
 
 
 def add_dummy_data():
     count = db_session().query(ReviewUser.id).count()
     if count == 0:
         log.info(f'Adding dummy data for {ReviewUser.__tablename__}...')
+        object_list = [
+            ReviewUser(
+                punctuation=5, comment='Molt bona gent!', order_group_id=1,
+                writer_id=user_service.get_id_by_name('Albert Suarez'),
+                user_id=user_service.get_id_by_name('Andreu Gallofre'),
+            )
+        ]
+        db_session().bulk_save_objects(object_list)
+        db_session().commit()
     else:
         log.info(f'Skipping dummy data for {ReviewUser.__tablename__} because is not empty.')
