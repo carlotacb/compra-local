2a2330616f6de2f6257f82c53ee291ba9506858d
Albert Suarez <alsumo95@gmail.com>
2020-04-10 17:28:03 +0200

    Added dummy data for User

--- api/src/service/user.py
+++ api/src/service/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy.exc import IntegrityError
 
 from src.db.sqlalchemy import db_session
+from src.enum.user_type import UserType
 from src.helper import image as image_util, log
 from src.model.user import User
 
@@ -9,6 +10,30 @@ def add_dummy_data():
     count = db_session().query(User.id).count()
     if count == 0:
         log.info(f'Adding dummy data for {User.__tablename__}...')
+        object_list = [
+            User(
+                name='Albert Suarez', email_address='hi@albert.dev',
+                password='72d0166b5707d129dc321e56692fe454c034552ee9e2b38f5a7f1c1306a632ea',
+                type=UserType.client, image=image_util.encode_and_resize('test/mock/user_image_1.jpg')
+            ),
+            User(
+                name='Andreu Gallofre', email_address='hi@andreu.dev',
+                password='79f2653ff4301ea86f566d4e1f4dcbef74ad6b8dd0b47e564bf570007d50cd70',
+                type=UserType.client, image=image_util.encode_and_resize('test/mock/user_image_2.jpg')
+            ),
+            User(
+                name='Elena Ruiz', email_address='hi@elena.dev',
+                password='0ce93c9606f0685bf60e051265891d256381f639d05c0aec67c84eec49d33cc1',
+                type=UserType.business, image=image_util.encode_and_resize('test/mock/user_image_3.jpg')
+            ),
+            User(
+                name='Carlota Catot', email_address='hi@carlota.dev',
+                password='332b7c12e4832aa8241acb324f2deaa4cac7a522243d1f078259fac18873bcce',
+                type=UserType.business, image=image_util.encode_and_resize('test/mock/user_image_4.jpg')
+            )
+        ]
+        db_session().bulk_save_objects(object_list)
+        db_session().commit()
     else:
         log.info(f'Skipping dummy data for {User.__tablename__} because is not empty.')
 
