5da2177b72e87a69ae21a2249e5f58fa894e551e
Carlota <carkbra@gmail.com>
2020-04-10 19:28:49 +0200

    added ReviewCard to routing index component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,10 +1,10 @@
 import { Sidebar as Sid } from './Sidebar/SideBar';
 import { SearchBox as SB} from './Box/SearchBox';
 import { ProfileBox as PB} from './Box/ProfileBox';
-import {ListView as LV} from './Listview/ListView';
-import { StoreCard as SC} from './Card/StoreCard';
+import { ListView as LV } from './Listview/ListView';
+import { StoreCard as SC } from './Card/StoreCard';
 import { ConfirmationDialog as CD } from './Dialog/ConfirmationDialog';
-
+import { ReviewCard as RC } from './Card/ReviewCard'
 //Store 
 import { StoreBody as StB } from './Store/StoreBody';
 import { StoreHeader as SH} from './Store/StoreHeader';
@@ -15,6 +15,6 @@ export const ListView = LV;
 export const StoreCard = SC;
 export const ProfileBox = PB;
 export const ConfirmationDialog = CD;
-
+export const ReviewCard = RC;
 export const StoreBody = StB;
 export const StoreHeader = SH;  
