16326260f2617da75240ac548fd0502dee0ed7da
Carlota <carkbra@gmail.com>
2020-04-11 16:59:28 +0200

    updated fake information

--- client/src/services/Orders/CompletedOrders.js
+++ client/src/services/Orders/CompletedOrders.js
@@ -4,7 +4,7 @@ export function getCompletedOrders(idStore) {
             {
                 "complition_date": Date(20/9/2020),
                 "local_name": "Bona fruita busquets",
-                "total": 53.20,
+                "total": 7.5,
                 "ticket": [
                     {
                         "product_name": "Pebrots",
@@ -15,6 +15,40 @@ export function getCompletedOrders(idStore) {
                         "product_name": "Maduixots del maresme",
                         "quantity": "4 unitats",
                         "total_price": 2.5
+                    }
+                ]
+            },
+            {
+                "complition_date": Date(20/9/2020),
+                "local_name": "Bona fruita sants",
+                "total": 3.5,
+                "ticket": [
+                    {
+                        "product_name": "Patates noves",
+                        "quantity": "300g",
+                        "total_price": 2
+                    },
+                    {
+                        "product_name": "Plàtans de canaries",
+                        "quantity": "100g",
+                        "total_price": 1.5
+                    }
+                ]
+            },
+            {
+                "complition_date": Date(20/9/2020),
+                "local_name": "Farmacia Valentines",
+                "total": 115,
+                "ticket": [
+                    {
+                        "product_name": "Paracetamol 1g",
+                        "quantity": "2 caixes",
+                        "total_price": 15
+                    },
+                    {
+                        "product_name": "Mascareta",
+                        "quantity": "1",
+                        "total_price": 100
                     }
                 ]
             }
