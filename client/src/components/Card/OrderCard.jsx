36302276eb24495ffe73b9ab6fe9cd2195efb099
Carlota <carkbra@gmail.com>
2020-04-11 10:59:54 +0200

    changed background color in ordercard

--- client/src/components/Card/OrderCard.jsx
+++ client/src/components/Card/OrderCard.jsx
@@ -7,7 +7,14 @@ import { VerticalStepper } from '../../components';
 const useStyles = makeStyles((theme) => ({
     root: {
         margin: theme.spacing(1),
-        padding: theme.spacing(2)
+        padding: theme.spacing(2),
+        backgroundColor: '#F9F9F9'
+    },
+    information: {
+        display: 'flex',
+	    flexDirection: 'column',
+	    justifyContent: 'space-between',
+        paddingLeft: '1.5em'
     },
     summary: {
         display: 'flex',
@@ -26,12 +33,12 @@ export function OrderCard(props) {
         return (
             [
                 <Grid item className={classes.summary}>
-                    <Typography variant="body1"> Pebrots 2kg </Typography>  
-                    <Typography variant="body1"> 5€ </Typography>
+                    <Typography variant="body2"> Pebrots 2kg </Typography>  
+                    <Typography variant="body2"> 5€ </Typography>
                 </Grid>,
                 <Grid item className={classes.summary}>
-                    <Typography variant="body1"> Pebrots 2kg </Typography>  
-                    <Typography variant="body1"> 5€ </Typography>
+                    <Typography variant="body2"> Maduixots del maresme </Typography>  
+                    <Typography variant="body2"> 2.5€ </Typography>
                 </Grid>
             ]
         )
@@ -41,17 +48,17 @@ export function OrderCard(props) {
         <Paper className={classes.root}>
             <Grid container direction="row">
                 <Grid item>
-                    <VerticalStepper />
+                    <VerticalStepper currentStep={2} steps={['Compra encomanada', 'Preparació', 'Per recollir', 'Recollit']}/>
                 </Grid>
                 <Grid item>
                     <Divider variant="middle" orientation="vertical" />
                 </Grid>
-                <Grid item>
+                <Grid item className={classes.information}>
                     <TertiaryButton> Pendent de Confirmació </TertiaryButton>
-                    <Typography variant="h6"> Comanda realitzada a: </Typography> 
+                    <Typography variant="body1"> Comanda realitzada a: </Typography> 
                     <Typography variant="h4"> Bona Fruita Busquets </Typography> 
                     <Typography variant="h5"> 53.20€ </Typography> 
-                    <Typography variant="h6"> Resum de la compra: </Typography> 
+                    <Typography variant="body1"> Resum de la compra: </Typography> 
                     {resumcompra()}
                 </Grid>
             </Grid>
