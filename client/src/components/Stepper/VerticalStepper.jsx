6b95b45adff1e0695c5356a23426a5998d32572f
Carlota <carkbra@gmail.com>
2020-04-11 10:15:14 +0200

    Created verticalStepper component

---
+++ client/src/components/Stepper/VerticalStepper.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const useStyles = makeStyles((theme) => ({
+    
+}));
+
+export function OrderCard(props) {
+    const classes = useStyles();
+
+    return (
+        <div>hola</div>
+    )
+}
