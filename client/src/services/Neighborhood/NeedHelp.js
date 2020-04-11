ba8dc2abaca56773ea39631600b27c95f013095f
Carlota <carkbra@gmail.com>
2020-04-12 00:23:32 +0200

    created mock for allpetitionswithhelper

---
+++ client/src/services/Neighborhood/NeedHelp.js
@@ -0,0 +1,48 @@
+export function NeedHelp(idStore) {
+    return new Promise((resolve, reject) => {
+        resolve([
+            {
+                "id": 1,
+                "order_list": [
+                {
+                    "id": 1,
+                    "name": "Bona Fruita Busquets",
+                    "postal_adress": "Carretera de sants, 258, 08028 Barcelona",
+                },
+                {
+                    "id": 2,
+                    "name": "Ferreteria paco",
+                    "postal_adress": "Carretera de sants, 230, 08028 Barcelona",
+                }
+                ],
+                "user": {
+                    "id": 1,
+                    "name": "Albert Suarez",
+                    "postal_adress": "Carrer de Sants, 337, 5-2",
+                    "type": "CLIENT"
+                }
+            },
+            {
+                "id": 2,
+                "order_list": [
+                {
+                    "id": 1,
+                    "name": "Bona Fruita Busquets",
+                    "postal_adress": "Carretera de sants, 258, 08028 Barcelona",
+                },
+                {
+                    "id": 2,
+                    "name": "Ferreteria paco",
+                    "postal_adress": "Carretera de sants, 230, 08028 Barcelona",
+                }
+                ],
+                "user": {
+                    "id": 1,
+                    "name": "Albert Suarez",
+                    "postal_adress": "Carrer de Sants, 337, 5-2",
+                    "type": "CLIENT"
+                }
+            }
+        ]);
+    });
+}
