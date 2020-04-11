5dadddc83bc1f8b2491442f83a02c266348c5122
Carlota <carkbra@gmail.com>
2020-04-11 10:00:11 +0200

    layed out information in OrderCard

--- client/src/components/Card/OrderCard.jsx
+++ client/src/components/Card/OrderCard.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import { Grid, Paper, Typography, makeStyles } from '@material-ui/core';
+import { TertiaryButton } from "../../shared-components";
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
         margin: theme.spacing(1),
         padding: theme.spacing(2)
+    },
+    summary: {
+        display: 'flex',
+	    flexDirection: 'row',
+	    flexWrap: 'nowrap',
+	    justifyContent: 'space-between',
+	    alignItems: 'stretch',
+	    alignContent: 'stretch',
     }
 }));
 
 export function OrderCard(props) {
     const classes = useStyles();
+
+    const resumcompra = () => {
+        return (
+            [
+                <Grid item className={classes.summary}>
+                    <Typography variant="body1"> Pebrots 2kg </Typography>  
+                    <Typography variant="body1"> 5€ </Typography>
+                </Grid>,
+                <Grid item className={classes.summary}>
+                    <Typography variant="body1"> Pebrots 2kg </Typography>  
+                    <Typography variant="body1"> 5€ </Typography>
+                </Grid>
+            ]
+        )
+    }
 
     return (
         <Paper className={classes.root}>
@@ -21,9 +45,12 @@ export function OrderCard(props) {
                     </Typography>  
                 </Grid>
                 <Grid item>
-                <Typography variant="h5">
-                        {'rest of information'}
-                    </Typography>  
+                    <TertiaryButton> Pendent de Confirmació </TertiaryButton>
+                    <Typography variant="h6"> Comanda realitzada a: </Typography> 
+                    <Typography variant="h4"> Bona Fruita Busquets </Typography> 
+                    <Typography variant="h5"> 53.20€ </Typography> 
+                    <Typography variant="h6"> Resum de la compra: </Typography> 
+                    {resumcompra()}
                 </Grid>
             </Grid>
         </Paper>
