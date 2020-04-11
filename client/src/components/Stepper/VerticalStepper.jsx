78bd20cb4a837461ca42b43e2fd73c4ca85edacb
Carlota <carkbra@gmail.com>
2020-04-11 10:30:25 +0200

    added props to VerticalStepper

--- client/src/components/Stepper/VerticalStepper.jsx
+++ client/src/components/Stepper/VerticalStepper.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  button: {
+    marginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing(2),
+  },
+  resetContainer: {
+    padding: theme.spacing(3),
+  },
+}));
 
 export function VerticalStepper(props) {
-    return (
-        <div>hola</div>
-    )
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = props.steps;
+
+  return (
+    <div className={classes.root}>
+      <Stepper activeStep={props.currentStep} orientation="vertical">
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </div>
+  );
 }
