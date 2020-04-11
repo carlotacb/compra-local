9bf9350a46e7cce666579cf407705ca1ba4e63ae
Carlota <carkbra@gmail.com>
2020-04-11 21:18:02 +0200

    corrected issue in ProcesHelper API mock

--- client/src/services/Neighborhood/ProcesHelper.js
+++ client/src/services/Neighborhood/ProcesHelper.js
@@ -1,37 +1,33 @@
-export function ProcesHelper(idStore) {
+export function getProcesHelper(idStore) {
     return new Promise((resolve, reject) => {
-        resolve([ {
-            "helper_list": [
+        resolve({
+                "id": 2,
+                "order_list": [
                 {
-                  "id": 2,
-                  "order_list": [
-                    {
-                      "id": 1,
-                      "name": "Bona Fruita Busquets",
-                      "postal_adress": "Carretera de sants, 258, 08028 Barcelona",
-                      "total": 53.60,
-                      "status": "En procès", 
-                    },
-                    {
-                        "id": 2,
-                        "name": "Ferreteria paco",
-                        "postal_adress": "Carretera de sants, 230, 08028 Barcelona",
-                        "total": 15.20,
-                        "status": "En procès", 
-                    }
-                  ],
-                  "total": 68.80,
-                  "user": {
+                    "id": 1,
+                    "name": "Bona Fruita Busquets",
+                    "postal_adress": "Carretera de sants, 258, 08028 Barcelona",
+                    "total": 53.60,
+                    "status": "En procès", 
+                },
+                {
+                    "id": 2,
+                    "name": "Ferreteria paco",
+                    "postal_adress": "Carretera de sants, 230, 08028 Barcelona",
+                    "total": 15.20,
+                    "status": "En procès", 
+                }
+                ],
+                "total": 68.80,
+                "user": {
                     "email_address": "hi@albert.dev",
                     "id": 1,
                     "name": "Albert Suarez",
                     "image": "algomoltmoltmoltmoltllarg",
                     "phone_number": "666 555 444",
                     "type": "CLIENT"
-                  }
                 }
-              ]
             }
-        ]);
+        );
     });
 }
