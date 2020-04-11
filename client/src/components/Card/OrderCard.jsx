04d7e3de1a2a03b2251e73679ebbb5975379dee7
Carlota <carkbra@gmail.com>
2020-04-11 12:44:29 +0200

    added props and best styles to order card in proccess

--- client/src/components/Card/OrderCard.jsx
+++ client/src/components/Card/OrderCard.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Divider, Grid, Paper, Typography, makeStyles } from '@material-ui/core';
-import { TertiaryButton } from "../../shared-components";
-import { VerticalStepper } from '../../components';
+import { Divider, Grid, Paper, Typography, makeStyles, Button } from '@material-ui/core';
+import { VerticalStepper } from "../../components";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -23,42 +22,84 @@ const useStyles = makeStyles((theme) => ({
 	    justifyContent: 'space-between',
 	    alignItems: 'stretch',
 	    alignContent: 'stretch',
+    },
+    bold: {
+        fontWeight: 'bold'
+    },
+    title: {
+        fontWeight: 'bold',
+        color: 'rgb(151, 119, 181)',
+    },
+    outlinedTag: {
+        ...theme.typography.button,
+        border: '1px solid rgb(151, 119, 181);',
+        padding: theme.spacing(1),
+        textAlign: 'center'
+    },
+    secondaryTag: {
+        ...theme.typography.button,
+        backgroundColor: '#F2B880',
+        padding: theme.spacing(1),
+        textAlign: 'center'
+    },
+    valorationButton: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(2),
     }
 }));
 
 export function OrderCard(props) {
     const classes = useStyles();
 
+    const getCurrentInformation = (currentStep) => {
+        switch(currentStep) {
+            case 0:
+                return <div className={classes.outlinedTag}> Pendent de Confirmació </div>;
+            case 1: 
+                return <div className={classes.outlinedTag}> Confirmat per l'establiment </div>;
+            case 2:
+                return <div className={classes.secondaryTag}> Ja pots recollir la teva comanda! </div>;
+            case 3:
+                return ( 
+                    <div>
+                        <Typography variant="body1" className={classes.title}> Com ha anat la compra? </Typography>
+                        <Typography variant="body2"> Si vols ajudar a la compra de futurs clients explica’ns que tal ha sigut la experiencia. </Typography>
+                        <Button variant="contained" color="primary" className={classes.valorationButton}> ESCRIU VALORACIÓ </Button>
+                    </div>
+                );
+        }
+    }
+
     const resumcompra = () => {
-        return (
-            [
+        const ticketAPI = props.ticket;
+        const ticket = [];
+
+        for (var i = 0; i < ticketAPI.length; ++i) {
+            ticket.push(
                 <Grid item className={classes.summary}>
-                    <Typography variant="body2"> Pebrots 2kg </Typography>  
-                    <Typography variant="body2"> 5€ </Typography>
-                </Grid>,
-                <Grid item className={classes.summary}>
-                    <Typography variant="body2"> Maduixots del maresme </Typography>  
-                    <Typography variant="body2"> 2.5€ </Typography>
+                    <Typography variant="body2"> {ticketAPI.product_name} - {ticketAPI.quantity} </Typography>  
+                    <Typography variant="body2"> {ticketAPI.total_price}€ </Typography>
                 </Grid>
-            ]
-        )
+            )    
+        }
+        return ticket
     }
 
     return (
         <Paper className={classes.root}>
             <Grid container direction="row">
                 <Grid item>
-                    <VerticalStepper currentStep={2} steps={['Compra encomanada', 'Preparació', 'Per recollir', 'Recollit']}/>
+                    <VerticalStepper currentStep={props.step} steps={['Compra encomanada', 'Preparació', 'Per recollir', 'Recollit']}/>
                 </Grid>
                 <Grid item>
                     <Divider variant="middle" orientation="vertical" />
                 </Grid>
                 <Grid item className={classes.information}>
-                    <TertiaryButton> Pendent de Confirmació </TertiaryButton>
-                    <Typography variant="body1"> Comanda realitzada a: </Typography> 
-                    <Typography variant="h4"> Bona Fruita Busquets </Typography> 
-                    <Typography variant="h5"> 53.20€ </Typography> 
-                    <Typography variant="body1"> Resum de la compra: </Typography> 
+                    {getCurrentInformation(3)}
+                    <Typography variant="body1" className={classes.bold}> Comanda realitzada a: </Typography> 
+                    <Typography variant="h4"> {props.local_name} </Typography> 
+                    <Typography variant="h5"> {props.total}€ </Typography> 
+                    <Typography variant="body1" className={classes.bold}> Resum de la compra: </Typography> 
                     {resumcompra()}
                 </Grid>
             </Grid>
