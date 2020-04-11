1b946542559890f0644bdd24a0467747ea50dfcc
Carlota <carkbra@gmail.com>
2020-04-11 13:37:51 +0200

    added helpers information in mock json

--- client/src/services/Orders/CurrentOrders.js
+++ client/src/services/Orders/CurrentOrders.js
@@ -19,7 +19,33 @@ export function getCurrentOrders(idStore) {
                             "quantity": "4 unitats",
                             "total_price": 2.5
                         }
-                    ]
+                    ],
+                    "assigned_helper": true,
+                    "helper": {
+                        "name": "Martí Marçal",
+                        "phone_number": 633049576,
+                        "payment": "Pagament Anticipat"
+                    }
+                },
+                {
+                    "delivery": false,
+                    "helper_needed": true,
+                    "local_name": "Bona fruita busquets",
+                    "step": 1,
+                    "total": 53.20,
+                    "ticket": [
+                        {
+                            "product_name": "Pebrots",
+                            "quantity": "2kg",
+                            "total_price": 5
+                        },
+                        {
+                            "product_name": "Maduixots del maresme",
+                            "quantity": "4 unitats",
+                            "total_price": 2.5
+                        }
+                    ],
+                    "assigned_helper": false
                 },
                 {
                     "delivery": false,
