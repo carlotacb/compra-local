6e0483864f4f7590a07582319bbd408f6316cfaa
Carlota <carkbra@gmail.com>
2020-04-11 23:52:24 +0200

    added more fake information to helper

--- client/src/services/Neighborhood/ProcesHelper.js
+++ client/src/services/Neighborhood/ProcesHelper.js
@@ -1,6 +1,7 @@
 export function getProcesHelper(idStore) {
     return new Promise((resolve, reject) => {
-        resolve([{
+        resolve([
+            {
                 "id": 2,
                 "order_list": [
                 {
@@ -27,8 +28,35 @@ export function getProcesHelper(idStore) {
                     "phone_number": "666 555 444",
                     "type": "CLIENT"
                 }
+            },
+            {
+                "id": 3,
+                "order_list": [
+                {
+                    "id": 1,
+                    "name": "Bona Fruita Busquets",
+                    "postal_adress": "Carretera de sants, 258, 08028 Barcelona",
+                    "total": 53.60,
+                    "status": "PICKED_UP", 
+                },
+                {
+                    "id": 2,
+                    "name": "Ferreteria paco",
+                    "postal_adress": "Carretera de sants, 230, 08028 Barcelona",
+                    "total": 15.20,
+                    "status": "PICKED_UP", 
+                }
+                ],
+                "total": 68.80,
+                "user": {
+                    "email_address": "hi@albert.dev",
+                    "id": 1,
+                    "name": "Albert Suarez",
+                    "image": "algomoltmoltmoltmoltllarg",
+                    "phone_number": "666 555 444",
+                    "type": "CLIENT"
+                }
             }
-            ]
-        );
+        ]);
     });
 }
