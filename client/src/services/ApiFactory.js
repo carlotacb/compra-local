902047338f07f3b69b3e899cbc0b62b965aed87b
Carlota <carkbra@gmail.com>
2020-04-11 17:38:54 +0200

    added GivenValorations to APIFactory

--- client/src/services/ApiFactory.js
+++ client/src/services/ApiFactory.js
@@ -2,12 +2,14 @@ import { getStoreInfo } from './Shop/Store';
 import { getCurrentOrders } from './Orders/CurrentOrders';
 import { getCompletedOrders } from './Orders/CompletedOrders';
 import { getRecivedValorations } from './Profile/RecivedValorations';
+import { getGivenValorations } from './Profile/GivenValorations';
 
 const endpoints = {
     'getStoreInfo': getStoreInfo,
     'getCurrentOrders': getCurrentOrders,
     'getCompletedOrders': getCompletedOrders,
     'getRecivedValorations': getRecivedValorations,
+    'getGivenValorations': getGivenValorations,
 }
 
 function get(name) {
