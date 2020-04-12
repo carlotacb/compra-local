bcce5a74b9377883c83d1c79e5bae65f9543c2d8
Carlota <carkbra@gmail.com>
2020-04-12 15:58:22 +0200

    added order_id and order_group_id

--- client/src/views/Orders/PendingReviewOrder.jsx
+++ client/src/views/Orders/PendingReviewOrder.jsx
@@ -7,14 +7,13 @@ export function PendingReviewOrder(props) {
     const getAllPendingOrders = () => {
         const resp = props.pendingReview;
         const orders = [];
-        console.log(resp)
 
         for (var i = 0; i < resp.length; ++i) {
             const order = resp[i];
             console.log(order)
-            orders.push(<PendingReviewCard local_name={order.local_name} helper_name={order.user_name} type={order.type}/>)
+            orders.push(<PendingReviewCard local_name={order.local_name} helper_name={order.user_name} type={order.type} order_id={order.order_id} order_group_id={order.order_group_id}/>)
         }
-
+        
         return orders
     }
 
