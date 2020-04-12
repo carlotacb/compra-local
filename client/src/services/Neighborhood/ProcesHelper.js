eeac39d82b8e346d30ff9b2875282c8a3f59e007
Carlota <carkbra@gmail.com>
2020-04-12 18:06:26 +0200

    deleted logs

--- client/src/services/Neighborhood/ProcesHelper.js
+++ client/src/services/Neighborhood/ProcesHelper.js
@@ -2,14 +2,16 @@ import { urlProd } from '../ApiFactory';
 const axios = require('axios');
 
 export function getProcesHelper(idStore) {
+    console.log(idStore)
     const endpoint = '/order/helping/user/' + idStore;
-
+    console.log(endpoint)
     return new Promise((resolve, reject) => {
         try {
             axios({
                 method: 'get',
                 url: urlProd + endpoint
             }).then(function(response) {
+                console.log(response);
                 if(response.data['error']) {
                     resolve({
                         error: true,
@@ -21,6 +23,7 @@ export function getProcesHelper(idStore) {
                         error: false,
                         list: response.data['response'].helping_order_list
                     });
+                    console.log(response.data['response'].helping_order_list)
                 }
             })
             .catch((err) => {
