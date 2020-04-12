caabd10bf735ad5e489683cbf6d9db0902b9f8b7
Carlota <carkbra@gmail.com>
2020-04-12 04:19:01 +0200

    integrate API call in progress orders view component

--- client/src/views/Orders/ProcessOrders.jsx
+++ client/src/views/Orders/ProcessOrders.jsx
@@ -11,7 +11,7 @@ export function ProcessOrders() {
     React.useEffect(function getStoreInfo() {
         const getCurrentOrdersAPI = ApiFactory.get('getCurrentOrders');
         getCurrentOrdersAPI(cookies.iusha).then((res) => {
-            setCurrentOrders(res);
+            setCurrentOrders(res.orders);
         });
     }, []);
 
@@ -21,12 +21,14 @@ export function ProcessOrders() {
         const orders = [];
 
         for (var i = 0; i < resp.length; ++i) {
+            const order = resp[i].order_list[0];
+
             if (resp[i].helper_needed) {
-                orders.push(<OrderHelpCard step={resp[i].step} local_name={resp[i].local_name} total={resp[i].total} ticket={resp[i].ticket} assigned_helper={resp[i].assigned_helper} helper={resp[i].helper}/>)
-            } else if (resp[i].delivery) {
-                orders.push(<OrderCard delivery={true} step={resp[i].step} local_name={resp[i].local_name} total={resp[i].total} ticket={resp[i].ticket}/>)
+                orders.push(<OrderHelpCard step={order.step} local_name={order.local_name} total={order.total} ticket={order.ticket} assigned_helper={resp[i].assigned_helper} helper={resp[i].helper}/>)
+            } else if (order.delivery) {
+                orders.push(<OrderCard delivery={true} step={order.step} local_name={order.local_name} total={order.total} ticket={order.ticket}/>)
             } else {
-                orders.push(<OrderCard delivery={false} step={resp[i].step} local_name={resp[i].local_name} total={resp[i].total} ticket={resp[i].ticket}/>)
+                orders.push(<OrderCard delivery={false} step={order.step} local_name={order.local_name} total={order.total} ticket={order.ticket}/>)
             }
         }
         return orders
