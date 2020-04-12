004b76985a37507cbfbbb84e0854870202a1d8b8
Carlota <carkbra@gmail.com>
2020-04-12 03:10:08 +0200

    created updateUserInfo endpoint

---
+++ client/src/services/Profile/UpdateUserInfo.js
@@ -0,0 +1,40 @@
+import { urlProd } from '../ApiFactory';
+const axios = require('axios');
+
+export function updateUserInfo(idStore, data) {
+    const endpoint = '/user/' + idStore ;
+    console.log(endpoint)
+
+    return new Promise((resolve, reject) => {
+        try {
+            axios({
+                method: 'put',
+                url: urlProd + endpoint,
+                body: data
+            }).then(function(response) {
+                console.log(response)
+                if(response.data['error']) {
+                    resolve({
+                        error: true,
+                        message: response.data['message']
+                    });
+                }
+                else {
+                    resolve({
+                        error: false,
+                        user: response.data['response']
+                    });
+                }
+            })
+            .catch((err) => {
+                console.log(err.message)
+                resolve({
+                    error: true,
+                    message: err.message
+                });
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
