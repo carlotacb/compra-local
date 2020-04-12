fbe2d3bc18e61547bdf995c7260e1edfe32b709e
Carlota <carkbra@gmail.com>
2020-04-12 15:57:03 +0200

    added error case

--- client/src/components/Dialog/ValorationDialog.jsx
+++ client/src/components/Dialog/ValorationDialog.jsx
@@ -15,8 +15,8 @@ export function ValorationDialog(props) {
         <Dialog fullWidth open={props.open} onClose={props.close} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description" >
             <DialogTitle id="form-dialog-title">{props.title}</DialogTitle>
                 <DialogContent>
-                    <TextField autoFocus margin="dense" id="punctuation" label="Valoració del 0 al 5" type="number" fullWidth InputProps={{ inputProps: { min: 0, max: 5 } }} onChange={(e) => setPunctuation(e.target.value)}/>
-                    <TextField autoFocus margin="dense" id="punctuation" label="Comentari" type="text" fullWidth rows={2} onChange={(e) => setComment(e.target.value)} />
+                    <TextField autoFocus error={props.error} margin="dense" id="punctuation" label="Valoració del 0 al 5" type="number" fullWidth InputProps={{ inputProps: { min: 0, max: 5 } }} onChange={(e) => setPunctuation(e.target.value)}/>
+                    <TextField error={props.error} margin="dense" id="punctuation" label="Comentari" type="text" fullWidth rows={2} onChange={(e) => setComment(e.target.value)} />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={() => handleAccept()} color="primary">
