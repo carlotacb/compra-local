891022e70154a11f0cf93117d3b0a924f7a8174c
Carlota <carkbra@gmail.com>
2020-04-12 15:30:31 +0200

    created API endpoint for valorate locals

---
+++ client/src/services/Orders/valorateLocal.js
@@ -0,0 +1,38 @@
+import { urlProd } from '../ApiFactory';
+const axios = require('axios');
+
+export function valorateLocal(data) {
+    const endpoint = '/review/local';
+
+    return new Promise((resolve, reject) => {
+        try {
+            axios({
+                method: 'post',
+                url: urlProd + endpoint,
+                data: data
+            }).then(function(response) {
+                console.log(response)
+                if(response.data['error']) {
+                    resolve({
+                        error: true,
+                        message: response.data['message']
+                    });
+                }
+                else {
+                    resolve({
+                        error: false,
+                        orders: response.data['response'].pending_order_list
+                    });
+                }
+            })
+            .catch((err) => {
+                resolve({
+                    error: true,
+                    message: err.message
+                });
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
