cba1d26f732a3e5e19f9367ab61dec58a362f980
Carlota <carkbra@gmail.com>
2020-04-12 15:17:26 +0200

    pending review card also done, without API for valorations

--- client/src/components/Card/PendingReviewCard.jsx
+++ client/src/components/Card/PendingReviewCard.jsx
@@ -12,9 +12,11 @@ const useStyles = makeStyles((theme) => ({
     },
     information: {
         display: 'flex',
-	    flexDirection: 'column',
+	    flexDirection: 'row',
+	    flexWrap: 'wrap',
 	    justifyContent: 'space-between',
-        paddingLeft: '1.5em'
+	    alignItems: 'center',
+	    alignContent: 'flex-start'
     },
     bold: {
         fontWeight: 'bold'
@@ -23,34 +25,74 @@ const useStyles = makeStyles((theme) => ({
         fontWeight: 'bold',
         color: 'rgb(151, 119, 181)',
     },
+    title2: {
+        fontWeight: 'bold',
+        color: '#F2B880',
+    },
     valorationButton: {
         marginTop: theme.spacing(1),
         marginBottom: theme.spacing(2),
     },
     uppercase: {
-        textTransform: 'uppercase'
+        textTransform: 'uppercase',
+        fontWeight: 'bold'
     }
 }));
 
-export function OrderCard(props) {
+export function PendingReviewCard(props) {
     const classes = useStyles();
     const [openModal, setOpenModal] = React.useState(false);
+    const [openModalVoluteer, setOpenModalVoluteer] = React.useState(false);
 
-    const handleValorateSell = (punctuation, valoration) =>{
+    const handleValorateSell = (punctuation, valoration) => {
         console.log(punctuation + " - " + valoration);
         setOpenModal(false);
+    }
+
+    const handleValorateVolunteer = (punctuation, valoration) => {
+        console.log(punctuation + " - " + valoration);
+        setOpenModalVoluteer(false);
+    }
+
+    const getCardType = () => {
+        if (props.type === "CLIENT") {
+            return (
+                <Grid item xs={12}>
+                    <Typography variant="h4" className={classes.title2}> Que tal el teu voluntari? </Typography> 
+                    <Grid item xs={12} className={classes.information}>
+                        <Grid item={8}>
+                            <Typography variant="h5" className={classes.uppercase}> {props.helper_name} </Typography>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Button variant="contained" color="secondary" className={classes.valorationButton} onClick={() => setOpenModalVoluteer(true)}> VALORA AL VOLUNTARI </Button>
+                        </Grid>
+                    </Grid> 
+                </Grid>
+            )
+        } else {
+            return (
+                <Grid item xs={12}>
+                    <Typography variant="h4" className={classes.title}> Com anat la teva compra? </Typography> 
+                    <Grid item xs={12} className={classes.information}>
+                        <Grid item={8}>
+                            <Typography variant="h5" className={classes.uppercase}> {props.local_name} </Typography>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Button variant="contained" color="primary" className={classes.valorationButton} onClick={() => setOpenModal(true)}> VALORA AL LOCAL </Button>
+                        </Grid>
+                    </Grid> 
+                </Grid>
+            )
+        }
     }
 
     return (
         <Paper className={classes.root}>
             <Grid container direction="row">
-                <Grid item xs={12} className={classes.information}>
-                    <Typography variant="h4" className={classes.title}> Com anat la teva compra? </Typography> 
-                    <Typography variant="h5" className={classes.uppercase}> {props.local_name} </Typography>
-                    <Button variant="contained" color="primary" className={classes.valorationButton} onClick={() => setOpenModal(true)}> VALORA AL LOCAL </Button>
-                </Grid>
+                {getCardType()}
             </Grid>
             <ValorationDialog open={openModal} title="Valora la teva compra" onAccept={(punct, comm) => handleValorateSell(punct, comm)} onClose={() => setOpenModal(false)} />
+            <ValorationDialog open={openModalVoluteer} title="Valora al teu voluntari" onAccept={(punct, comm) => handleValorateVolunteer(punct, comm)} onClose={() => setOpenModalVoluteer(false)} />
         </Paper>
     )
 }
